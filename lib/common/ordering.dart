10027d9607487cbc3ebef4780c4d23e80fd7b5a4
Roman Volovoy <romanvolovoy@gmail.com>
2025-03-15 04:05:25 +0100

    tests work but idk

---
+++ lib/common/ordering.dart
@@ -0,0 +1,172 @@
+import 'dart:collection';
+
+class Node<T> {
+  final T id;
+  Node<T>? next;
+  Node<T>? prev;
+
+  Node(this.id, {this.next, this.prev});
+}
+
+class Ordering<T> {
+  final Map<T, Node<T>> _map;
+  Node<T>? _last;
+
+  Ordering() : _map = {};
+
+  int get count => _map.length;
+
+  // optimized, faster then insert
+  void append(T id) {
+    final newNode = Node(id);
+    _map[id] = newNode;
+
+    if (_last == null) {
+      _last = newNode;
+      return;
+    }
+
+    newNode.prev = _last;
+    _last!.next = newNode;
+    _last = newNode;
+    return;
+  }
+
+  Node<T> _cut(T id, {required bool removeFromMap}) {
+    final current = _map[id];
+    assert(current != null);
+
+    final before = current!.prev;
+    final after = current.next;
+
+    before?.next = after;
+    after?.prev = before;
+
+    // cutting the first value
+    if (after == null) {
+      _last = before;
+    }
+
+    if (removeFromMap) {
+      _map.remove(id);
+      current.prev = null;
+      current.next = null;
+    }
+
+    return current;
+  }
+
+  void _paste(Node<T> node, T? afterId, {required bool addToMap}) {
+    if (addToMap) {
+      _map[node.id] = node;
+    }
+
+    if (afterId == null) {
+      final last = getLast();
+
+      node.next = last;
+      last.prev = node;
+
+      return;
+    }
+    final before = _map[afterId];
+
+    final after = before!.next;
+    before.next = node;
+    node.prev = before;
+    node.next = after;
+    after?.prev = node;
+
+    if (after == null) {
+      // adding to the end
+      _last = node;
+    }
+  }
+
+  void remove(T id) {
+    _cut(id, removeFromMap: true);
+  }
+
+  /// if afterId is null, the value will be inserted in the beginning
+  void insert(T id, T? afterId) {
+    if (_last == null) {
+      append(id);
+      return;
+    }
+
+    final node = Node<T>(id);
+    _paste(node, afterId, addToMap: true);
+  }
+
+  void move(T id, T? afterId) {
+    if (id == afterId) {
+      throw ArgumentError("cannot move in place, this is noop");
+    }
+    final cutNode = _cut(id, removeFromMap: false);
+    _paste(cutNode, afterId, addToMap: false);
+  }
+
+  // this will iterate all of them and return starting index
+  // for now its not optimized, but its okay
+  // empty list is not allowed
+  Node getAtIndex(int idx) {
+    assert(_last != null, 'empty list not allowed');
+    assert(_map.isNotEmpty, 'empty list not allowed');
+
+    var node = _last;
+    for (var i = 0; i < idx; i++) {
+      if (node == null) {
+        throw RangeError('ordering index $idx is out of range. max is $i');
+      }
+      node = node.prev;
+    }
+    return node!;
+  }
+
+  // this cant be called if there are no items...
+  Node<T> getLast() {
+    assert(_last != null && _map.isNotEmpty);
+    if (_last == null) {
+      throw StateError('No items in list');
+    }
+    var node = _last;
+
+    while (node!.prev != null) {
+      node = node.prev;
+    }
+    return node;
+  }
+
+  void clear() {
+    var node = _last;
+    while (node != null) {
+      _last!.next = null;
+      node = node.prev;
+    }
+    _map.clear();
+  }
+
+  // from last to first
+  List<T> toListDesc() {
+    if (_last == null) {
+      return [];
+    }
+
+    final result = List<T>.empty(growable: true);
+
+    var remaining = count;
+    var node = _last;
+    while (node != null) {
+      result.add(node.id);
+      node = node.prev;
+
+      remaining--;
+      assert(
+        remaining >= 0,
+        'infinite loop detected. count $count. list ${result.join(', ')} ',
+      );
+    }
+
+    return result;
+  }
+}
--- test/ordering_test.dart
+++ test/ordering_test.dart
@@ -1,5 +1,5 @@
 import 'package:notes_v0_2/common/id.dart';
-import 'package:notes_v0_2/notes/model_ordering.dart';
+import 'package:notes_v0_2/common/ordering.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -28,6 +28,7 @@ void main() {
       expect(list, equals([id3, id2, id1]));
       expect(ordering.count, equals(3));
     });
+
     test('remove', () {
       ordering.remove(id2);
 
@@ -35,6 +36,33 @@ void main() {
       expect(list, equals([id3, id1]));
       expect(ordering.count, equals(2));
     });
+    test('remove first', () {
+      ordering.remove(id1);
+
+      final list = ordering.toListDesc();
+      expect(list, equals([id3, id2]));
+      expect(ordering.count, equals(2));
+    });
+    test('remove last', () {
+      ordering.remove(id3);
+
+      final list = ordering.toListDesc();
+      expect(list, equals([id2, id1]));
+      expect(ordering.count, equals(2));
+    });
+    test('remove till empty', () {
+      ordering.remove(id3);
+      ordering.remove(id2);
+      ordering.remove(id1);
+
+      final list = ordering.toListDesc();
+      expect(list, equals([]));
+      expect(ordering.count, equals(0));
+
+      ordering.append(id4);
+      expect(ordering.toListDesc(), equals([id4]));
+    });
+
     test('insert', () {
       ordering.insert(id4, id2);
 
@@ -42,13 +70,30 @@ void main() {
       expect(list, equals([id3, id4, id2, id1]));
       expect(ordering.count, equals(4));
     });
-    test('insert at end', () {
+    test('insert first', () {
       ordering.insert(id4, null);
 
       final list = ordering.toListDesc();
       expect(list, equals([id3, id2, id1, id4]));
       expect(ordering.count, equals(4));
     });
+    test('insert last (append)', () {
+      ordering.insert(id4, id3);
+
+      final list = ordering.toListDesc();
+      expect(list, equals([id4, id3, id2, id1]));
+      expect(ordering.count, equals(4));
+    });
+    test('insert from empty', () {
+      final fresh = Ordering();
+      fresh.insert(id1, null);
+      fresh.insert(id2, id1);
+
+      final list = fresh.toListDesc();
+      expect(list, equals([id2, id1]));
+      expect(fresh.count, equals(2));
+    });
+
     test('move', () {
       ordering.append(id4);
       ordering.move(id2, id3);
@@ -60,6 +105,7 @@ void main() {
     test('move inplace prohibited', () {
       expect(() => ordering.move(id2, id2), throwsArgumentError);
     });
+
     test('getAtIndex', () {
       final node = ordering.getAtIndex(1);
       expect(node.id, equals(id2));
